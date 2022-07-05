70119ba424072fb655cd4b9fc2ed73b1cbce9ebc
adamaripici <55006272+adamaripici@users.noreply.github.com>
2022-07-05 13:49:12 -0500

    database schema setup.

--- student_store_backend/student-store.sql
+++ student_store_backend/student-store.sql
@@ -1,11 +1,11 @@
-\echo 'Delete and recreate auth_starter db?'
+\echo 'Delete and recreate student-store db?'
 \prompt 'Return for yes or control-C to cancel > ' answer
 
-DROP DATABASE auth_starter;
-CREATE DATABASE auth_starter;
-\connect auth_starter
+DROP DATABASE student_store;
+CREATE DATABASE student_store;
+\connect student_store
 
-\i auth-starter-schema.sql
+\i student-store-schema.sql
 
 \echo 'Delete and recreate auth_starter_test db?'
 \prompt 'Return for yes or control-C to cancel > ' answer
@@ -14,4 +14,4 @@ DROP DATABASE auth_starter_test;
 CREATE DATABASE auth_starter_test;
 \connect auth_starter_test
 
-\i auth-starter-schema.sql
+\i student-store-schema.sql
